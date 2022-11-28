25244030735d90e1100dc3e838d1d1fb460d80a5
Anton Sto <asto1387@gmail.com>
2022-11-28 15:59:09 -0500

    add marketplace assets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrate, render } from 'react-dom';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+// if (rootElement.hasChildNodes()) {
+  // hydrateRoot(rootElement, <App />);
+// } else {
+  // let root = createRoot(rootElement);
+  // root.render(<App />);
+// }
+
+// package git example
+// if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+// } else {
+//   let root = createRoot(rootElement);
+//   root.render(<App />);
+// }
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
